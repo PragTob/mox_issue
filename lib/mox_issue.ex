aebc654b985c39d900f3d0bd31b957f8653bfbba
Tobias Pfeiffer <pragtob@gmail.com>
2019-09-30 15:23:27 +0200

    empty basics

---
+++ lib/mox_issue.ex
@@ -0,0 +1,18 @@
+defmodule MoxIssue do
+  @moduledoc """
+  Documentation for MoxIssue.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MoxIssue.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mox_issue_test.exs
@@ -0,0 +1,8 @@
+defmodule MoxIssueTest do
+  use ExUnit.Case
+  doctest MoxIssue
+
+  test "greets the world" do
+    assert MoxIssue.hello() == :world
+  end
+end
